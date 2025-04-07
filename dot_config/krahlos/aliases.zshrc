fdd2ad4ea0c955d8e9e167dfc58ac428d1f225f8
krahlos <marco-krahl@web.de>
2025-04-07 16:34:11 +0200

    refactor(krahlos): update config files

---
+++ dot_config/krahlos/aliases.zshrc
@@ -0,0 +1,8 @@
+alias ls="ls -hFG"
+alias l="ls -lF"
+alias ll="ls -alF"
+alias lt="ls -ltrF"
+alias lls="ls -alSrF"
+alias llt="ls -altrF"
+
+alias cdtmp=cd "$(mktemp -d)"
--- dot_config/krahlos/functions.sh
+++ dot_config/krahlos/functions.sh
@@ -1,11 +1,24 @@
-alias ls="ls -hFG"
-alias l="ls -lF"
-alias ll="ls -alF"
-alias lt="ls -ltrF"
-alias lls="ls -alSrF"
-alias llt="ls -altrF"
+#!/usr/bin/env bash
 
-alias cdtmp=cd "$(mktemp -d)"
+colortest() {
+  # https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+
+  T='gYw' # The test text
+
+  echo -e "               40m     41m     42m     43m\
+       44m     45m     46m     47m";
+
+  for FGs in '  m' ' 1m' '30m' '90m' '31m' '91m' '32m' \
+             '92m' '33m' '93m' '34m' '94m' '35m' '95m' \
+             '36m' '96m' '37m' '97m';
+    do FG=${FGs// /}
+    echo -en " $FGs \033[$FG  $T  "
+    for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+      do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+    done
+    echo;
+  done
+}
 
 passgen() {
   local block_length=6
